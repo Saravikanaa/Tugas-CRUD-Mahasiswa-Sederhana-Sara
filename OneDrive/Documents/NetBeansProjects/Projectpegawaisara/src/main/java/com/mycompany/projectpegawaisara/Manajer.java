afd04e39093e18aaaf9443bd9de315f319e8652a
Sara Vikana <SARA VIKANA BANGUN@LAPTOP-JP1G78G2>
2022-11-19 17:33:43 +0700

    Sara Vikana br Bangun_21103092_Modul4

---
+++ OneDrive/Documents/NetBeansProjects/Projectpegawaisara/src/main/java/com/mycompany/projectpegawaisara/Manajer.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectpegawaisara;
+
+/**
+ *
+ * @author SARA VIKANA BANGUN
+ */
+class Manajer {
+
+    void setManajer(String adi, String string, String jl_Angsa_56, int i, int i0, String keuangan) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void cetakManajer() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
