9ddd2c6927670c41b4a250935d9210e8c6c5f973
Sara Vikana <SARA VIKANA BANGUN@LAPTOP-JP1G78G2>
2022-11-22 10:33:13 +0700

    Sara Vikana br Bangun_21103092_Modul5 Mobil

---
+++ OneDrive/Documents/NetBeansProjects/ProjectMobil/src/main/java/com/mycompany/projectmobil/ProjectMobil.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.projectmobil;
+
+/**
+ *
+ * @author SARA VIKANA BANGUN
+ */
+public class ProjectMobil {
+
+    public static void main(String[] args) {
+       System.out.println("sedan");
+
+        Sedan h = new Sedan();
+        h.tampilkan();
+
+        System.out.println("\nminiBus");
+
+        MiniBus t = new MiniBus();
+        t.tampilkan();
+
+        System.out.println("\nBus");
+        
+        Bus z = new Bus();
+        z.tampilkan();
+ }
+
+    
+}
