1b14255e91fd8637f8e912b769af27cf6401bd99
Sara Vikana <SARA VIKANA BANGUN@LAPTOP-JP1G78G2>
2022-11-22 09:32:50 +0700

    Sara Vikana br Bangun_21103092_Modul4

---
+++ OneDrive/Documents/NetBeansProjects/ProjectSpeak/src/main/java/com/mycompany/projectspeak/Owl.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectspeak;
+
+/**
+ *
+ * @author SARA VIKANA BANGUN
+ */
+public class Owl extends Mammal{
+
+    String name;
+ public void speak()
+ {
+ System.out.println("Whoo! Whoo!");
+ }
+ public void sleep()
+ {
+ System.out.println(" "); // Owl makes no noise when sleeping
+ }
+}
