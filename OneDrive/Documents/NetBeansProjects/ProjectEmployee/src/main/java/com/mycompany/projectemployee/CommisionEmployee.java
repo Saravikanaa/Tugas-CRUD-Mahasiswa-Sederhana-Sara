4bb36ac06984220c8a81e25b9afc9d1d22d9aaeb
Sara Vikana <SARA VIKANA BANGUN@LAPTOP-JP1G78G2>
2022-11-29 19:55:25 +0700

    UTS_NO2_A_3092

---
+++ OneDrive/Documents/NetBeansProjects/ProjectEmployee/src/main/java/com/mycompany/projectemployee/CommisionEmployee.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectemployee;
+
+/**
+ *
+ * @author SARA VIKANA BANGUN
+ */
+public class CommisionEmployee extends Employee {
+    public float Komisi;
+    public float TotalPenjualan;
+    public float Totalgaji;
+    
+    public CommisionEmployee(){
+        
+    }
+    
+    public float TotalGaji(){
+        Totalgaji = GajiPokok + (Komisi * TotalPenjualan);
+        return Totalgaji;
+    }
+    
+    public void TampilData(){
+        System.out.println("Commission Employee");
+        Tampil();
+        System.out.println("Total Gaji: " + Totalgaji);
+    }
+}
+
+    
+
