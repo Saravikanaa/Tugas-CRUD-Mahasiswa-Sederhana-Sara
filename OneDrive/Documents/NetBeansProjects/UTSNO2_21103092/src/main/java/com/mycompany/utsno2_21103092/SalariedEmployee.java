a560406ddbf277488ee0dc1a7dffcaa30a93c9de
Sara Vikana <SARA VIKANA BANGUN@LAPTOP-JP1G78G2>
2022-12-11 06:51:23 +0700

    UTS_NO2_A_3092

---
+++ OneDrive/Documents/NetBeansProjects/UTSNO2_21103092/src/main/java/com/mycompany/utsno2_21103092/SalariedEmployee.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.utsno2_21103092;
+
+/**
+ *
+ * @author SARA VIKANA BANGUN
+ */
+public class SalariedEmployee extends Employee {
+    public SalariedEmployee(){
+        
+    }
+    
+    public void TampilData(){
+        System.out.println("Salaried Employee");
+        Tampil();
+        System.out.println("Total Gaji: " + GajiPokok);
+    }
+}
+
+    
