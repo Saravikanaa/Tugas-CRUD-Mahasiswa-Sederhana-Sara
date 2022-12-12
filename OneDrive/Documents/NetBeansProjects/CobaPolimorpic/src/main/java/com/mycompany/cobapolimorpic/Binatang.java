5f1eaabe67d264c352262524b3e3a1699a69cba0
Sara Vikana <SARA VIKANA BANGUN@LAPTOP-JP1G78G2>
2022-12-12 10:15:13 +0700

    Laporan Praktikum modul 6_Sara Vikana

---
+++ OneDrive/Documents/NetBeansProjects/CobaPolimorpic/src/main/java/com/mycompany/cobapolimorpic/Binatang.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cobapolimorpic;
+
+/**
+ *
+ * @author SARA VIKANA BANGUN
+ */
+public abstract class Binatang {
+   Binatang(String jenis)
+{
+this.jenis=jenis;
+}
+protected abstract void suara(); //Deklarasi,tidak didefisikan
+public String toString()
+{
+return "Seekor "+jenis;
+}
+private String jenis;
+
+    private static class didefisikan {
+
+        public didefisikan() {
+        }
+    }
+
+}
