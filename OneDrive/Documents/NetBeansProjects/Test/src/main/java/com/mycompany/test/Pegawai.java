e324be51918fa71cd067032e8d630ca13ebacad2
Sara Vikana <SARA VIKANA BANGUN@LAPTOP-JP1G78G2>
2022-12-12 10:17:47 +0700

    Laporan Praktikum modul 6_Sara Vikana

---
+++ OneDrive/Documents/NetBeansProjects/Test/src/main/java/com/mycompany/test/Pegawai.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.test;
+
+/**
+ *
+ * @author SARA VIKANA BANGUN
+ */
+public abstract class Pegawai {
+   private String namaPeg;
+//konstruktor
+public Pegawai(String nama)
+{
+namaPeg=nama;
+}
+//method (get) untuk mengembalikan nama pegawai
+public String namaPegawai()
+{
+return namaPeg;
+}
+//Method abstrak ini diwariskan ke semua kelas yang diturunkan dari kelas abstrak ini
+public abstract double income();
+ 
+}
