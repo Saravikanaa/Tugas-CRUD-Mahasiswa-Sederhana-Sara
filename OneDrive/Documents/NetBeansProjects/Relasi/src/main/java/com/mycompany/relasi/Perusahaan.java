a722ba8f174218b5d78f8064a006f8ad6d45058f
Sara Vikana <SARA VIKANA BANGUN@LAPTOP-JP1G78G2>
2022-12-19 22:50:22 +0700

    Laporan Praktikum modul 7_Relasi_Sara

---
+++ OneDrive/Documents/NetBeansProjects/Relasi/src/main/java/com/mycompany/relasi/Perusahaan.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.relasi;
+
+/**
+ *
+ * @author SARA VIKANA BANGUN
+ */
+// Class Perusahaan
+public class Perusahaan {
+    private String namaPer;
+private Pegawai peg[]; //Agregasi antara pegawai dan perusahaan
+private int counter;
+public Perusahaan(String namaPer)
+{
+this.namaPer=namaPer;
+counter=0;
+peg = new Pegawai[3];
+System.out.println("Konstruktor perusahaan dijalankan...");
+}
+//Relasi agregasi antara pegawai dan perusahaan
+public void insertPegawai(Pegawai p)
+{
+peg[counter]=p;
+counter++;
+}
+public void tampilPer()
+{
+System.out.println("Perusahaan "+namaPer+" memiliki pegawai : ");
+for(int i=0; i<counter; i++)
+{
+peg[i].tampilPeg();
+}
+}
+}
